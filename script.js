d3e0b5c87f63f9226f5a325cf680f326f536dcda
zixuan peng <zixuan.peng@gmail.com>
2024-08-25 19:58:49 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.navbar a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
